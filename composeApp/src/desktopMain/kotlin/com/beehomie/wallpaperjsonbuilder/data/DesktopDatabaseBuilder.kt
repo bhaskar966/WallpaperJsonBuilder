9c714019a9ffd7b5f5e802796dd0127e0b4d91a5
bhaskar966 <deybhaskar999@gmail.com>
2025-08-15 14:54:02 +0530

    Fixed database location

--- composeApp/src/desktopMain/kotlin/com/beehomie/wallpaperjsonbuilder/data/DesktopDatabaseBuilder.kt
+++ composeApp/src/desktopMain/kotlin/com/beehomie/wallpaperjsonbuilder/data/DesktopDatabaseBuilder.kt
@@ -7,7 +7,26 @@ import java.io.File
 
 fun desktopDatabaseBuilder(): RoomDatabase.Builder<WallpaperDB> {
 
-    val dbFile = File(System.getProperty("java.io.tempdir"), "room_db.db")
+//    val dbFile = File(System.getProperty("java.io.tempdir"), "room_db.db")
+
+    val osName = System.getProperty("os.name").lowercase()
+    val appName = "com.beehomie.wallpaperjsonbuilder"
+
+    val dbFile = when {
+        osName.contains("win") -> {
+            val appData = System.getenv("APPDATA") ?: "${System.getProperty("user.home")}\\AppData\\Roaming"
+            File(appData, "$appName\\room_db.db")
+        }
+        osName.contains("nix") || osName.contains("nux") -> {
+            val xdgDataHome = System.getenv("XDG_DATA_HOME") ?: "${System.getProperty("user.home")}/.local/share"
+            File(xdgDataHome, "$appName/room_db.db")
+        }
+        else -> {
+            File("room_db.db")
+        }
+    }
+
+    dbFile.parentFile?.mkdirs()
 
     return Room.databaseBuilder(
         dbFile.absolutePath
